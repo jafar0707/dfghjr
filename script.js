8013c0e76547cc9c50a05e5eb14a0adb6d4f25e1
jafar0707 <jafartolibjonov58@gmail.com>
2025-05-29 17:39:31 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-let text1 = document.querySelector(`.t1`)
-let text2 = document.querySelector(`.t2`)
-let text3 = document.querySelector(`.t3`)
-let di = document.querySelector(`div`)
+let a = prompt(`Write your name`)
+let text = document.createElement(`h1`)
+let body = document.querySelector(`body`)
 
-let a = prompt(`Familiya`)
-let b = prompt(`Ism`)
-let c = prompt(`Yosh`)
+body.appendChild(text)
 
-text1.innerHTML = `familiya: ${a}`
-text2.innerHTML = `ism: ${b}`
-text3.innerHTML = `yosh: ${c}`
-
-
-
-di.style.background = `black`
-
-text1.style.color = `white`
-text2.style.color = `white`
-text3.style.color = `white`
+text.textContent = a
+text.style.color = `blue`
+text.style.textAlign = `center`
+text.style.fontSize = `60px`
+body.style.background = `yellow`
+text.style.paddingTop = `220px`
